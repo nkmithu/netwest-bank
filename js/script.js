9859628b7494a326c44f6f2b5f43d13a4716363c
nkmithu <nkmithu93@gmail.com>
2020-07-24 00:27:31 +0600

    one

--- js/script.js
+++ js/script.js
@@ -1,9 +1,48 @@
-const loginBtn = document.getElementById("login");
-loginBtn.addEventListener("click", function(){
-    const loginArea = document.getElementById("login-area");
-    loginArea.style.display="none";
-    const transactionArea = document.getElementById("transaction-area");
+// login button event handler
 
-    transactionArea.style.display="block";
+const loginBtn = document.getElementById("login");
+loginBtn.addEventListener("click", function () {
+    const loginArea = document.getElementById("login-area");
+    loginArea.style.display = "none";
+    const transactionArea = document.getElementById("transaction-area");
+    transactionArea.style.display = "block";
 
 })
+
+//deposit button event handler
+const depositBtn = document.getElementById("addDeposit");
+depositBtn.addEventListener("click", function () {
+    const depositNumber = getInputNum("depositAmount");
+    
+    updateSpanText("currentDeposit", depositNumber);
+    updateSpanText("currentBalance", depositNumber);
+
+    document.getElementById("depositAmount").value = "";
+})
+
+//withdraw button event handler
+const withdrawBtn = document.getElementById("addWithdraw");
+withdrawBtn.addEventListener("click", function () {
+    const withdrawNumber = getInputNum("withdrawAmount");
+    getInputNum("withdrawAmount");
+    updateSpanText("currentWithdraw", withdrawNumber);
+    updateSpanText("currentBalance", -1 * withdrawNumber);
+
+    document.getElementById("withdrawAmount").value = "";
+})
+
+
+function getInputNum(id){
+    const amount = document.getElementById(id).value;
+    const amountNumber = parseFloat(amount);
+
+    return amountNumber;
+}
+
+function updateSpanText(id, depositNumber) {
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = depositNumber + currentNumber;
+
+    document.getElementById(id).innerText = totalAmount;
+}
